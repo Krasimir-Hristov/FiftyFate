bed1559fb7778f1ae894a2dfde58f8a81f13e126
krasi <krasimir.xristov@gmail.com>
2023-06-29 20:28:49 +0200

    create my bet boxes

--- fiftyfate/src/components/Controller.tsx
+++ fiftyfate/src/components/Controller.tsx
@@ -1,11 +1,23 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
+
+import Button from './Button';
+import BetBox from './BetBox';
 
 function Controller() {
+  const [hasWon, setHasWon] = useState(false);
+
+  const handlePlaceBet = () => {
+    setHasWon(!hasWon);
+  };
+
+  useEffect(() => {
+    console.log(hasWon);
+  }, [hasWon]);
+
   return (
-    <div className="container bg-green-500 mx-auto">
-      <button className="transition-all duration-300 border border-black w-full mt-4 px-4 py-2 rounded-sm bg-indigo-500 hover:bg-indigo-600 text-white ">
-        Place Bet
-      </button>
+    <div className="w-full md:w-[850px] lg:w-[1200px] py-12 bg-green-500 mx-auto px-5">
+      <Button runFunction={handlePlaceBet} />
+      <BetBox />
     </div>
   );
 }
