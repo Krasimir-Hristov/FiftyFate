a2d917cc91a37d0647e29f46dbb78ae2eb0de70a
krasi <krasimir.xristov@gmail.com>
2023-07-02 17:30:48 +0200

    Handle my winner or looser states

--- fiftyfate/src/components/Controller.tsx
+++ fiftyfate/src/components/Controller.tsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 import Button from './Button';
 import BetBox from './BetBox';
@@ -7,18 +8,48 @@ function Controller() {
   const [hasWon, setHasWon] = useState(false);
   const [betDirection, setBetDirection] = useState('up');
 
-  const handlePlaceBet = () => {
-    setHasWon(!hasWon);
+  const handlePlaceBet = async () => {
+    let isWinner = false;
+
+    const url =
+      'https://www.random.org/integers/?num=1&min=1&max=100&col=1&base=10&format=plain&rnd=new';
+
+    await axios
+      .get(url)
+      .then((res) => {
+        if (res.status === 200) {
+
+          const val = res.data;
+          console.log(val);
+          
+
+          if (val >= 50 && betDirection === 'up') {
+
+            isWinner = true;
+          } else if (val < 50 && betDirection === 'down') {
+
+            isWinner = true;
+          }
+
+        } else {
+          console.error('There was some kind of error getting the data.');
+        }
+      })
+      .catch((err) => {
+        console.log(err.data, err.message);
+      });
+
+    setHasWon(isWinner);
   };
 
   useEffect(() => {
-    console.log(betDirection);
-  }, [betDirection]);
+    console.log(hasWon);
+  }, [hasWon]);
 
   return (
     <div className="w-full md:w-[850px] lg:w-[1200px] py-12 mx-auto px-5">
       <Button runFunction={handlePlaceBet} />
-      <BetBox betDirection={betDirection} setBetDirection={setBetDirection}/>
+      <BetBox betDirection={betDirection} setBetDirection={setBetDirection} />
     </div>
   );
 }
